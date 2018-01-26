f939bea3d7ad2bae791e5a8241e44d25c4a55b06
NickGatti <nick.gatti@gmail.com>
2018-01-26 15:03:40 -0700

    gym status full now

--- controllers/gym.js
+++ controllers/gym.js
@@ -3,28 +3,21 @@ const knex = require( "../db/knex.js" );
 module.exports = {
 
     assign: function ( req, res, next ) {
-        let saveSessionFlag = false
-
         if ( !req.session.user.gym.p1 ) {
             req.session.user.gym.p1 = {
                 id: req.params.id
             }
-            saveSessionFlag = true
-        } else if ( !req.session.user.gym.p2 ) {
+        } else if ( !req.session.user.gym.p2 && req.session.user.gym.p1.id != req.params.id ) {
             req.session.user.gym.p2 = {
                 id: req.params.id
             }
-            saveSessionFlag = true
         } else {
             req.session.user.gym.full = true
         }
 
-        if ( saveSessionFlag ) {
-
-            req.session.save( function () {
-                res.redirect( '/pokemon' );
-            } )
-        }
+        req.session.save( function () {
+            res.redirect( '/pokemon' );
+        } )
     }
 
 };
